a3ce8ac8905721195c2dfb4f2653a89df2bde26a
KimJeonghun91 <suuuk3@naver.com>
2021-04-15 17:52:17 +0900

    수정

--- src/screens/Canvas.js
+++ src/screens/Canvas.js
@@ -2,10 +2,8 @@ import React, { useState, useEffect } from "react";
 import { StyleSheet, ScrollView, SafeAreaView, Platform, Alert, Text, TouchableOpacity } from "react-native";
 import BackgroundService from 'react-native-background-actions';
 import Loader from "../components/Loader"
-import * as MyAsyncStorage from "../constants/MyAsyncStorage";
 import { accelerometer, gyroscope, setUpdateIntervalForType, SensorTypes } from "react-native-sensors";
 import { map, filter } from "rxjs/operators";
-
 
 // 업데이트 간격 조절
 setUpdateIntervalForType(SensorTypes.accelerometer, 1000); // defaults to 100ms
@@ -20,28 +18,30 @@ class Canvas extends React.Component {
             prevSpeed: 0,
             mStep: 0,
             subscription: null,
+            isSubState: false
         };
     }
 
     componentDidMount() {
-        // this._startWalk()
+        this._startWalk()
     }
 
-    // async componentWillUnmount() {
-    //     this._stopWalk()
-    // }
+    async componentWillUnmount() {
+        this._stopWalk()
+    }
 
     _stopWalk = async () => {
-        await MyAsyncStorage._writeAsyncStorage("AS_STEP_CNT", { mStep: 0, prevSpeed: 0 });
         await BackgroundService.stop();
-        // if (this.state.subscription !== null) {
-        //     this.state.subscription.unsubscribe();
-        //     this.state.subscription = null
-        // }
+        this.setState({ isSubState: false })
+        if (this.state.subscription !== null) {
+            this.state.subscription.unsubscribe();
+            this.state.subscription = null
+        }
     }
 
 
     _startWalk = async () => {
+        console.log("_startWalk : BackgroundService.isRunning() : " + BackgroundService.isRunning())
         if (!BackgroundService.isRunning()) {
             // activateKeepAwake();
             await BackgroundService.start(this.veryIntensiveTask, {
@@ -56,7 +56,7 @@ class Canvas extends React.Component {
                 // linkingURI: 'yourSchemeHere://chat/jane', // See Deep Linking for more info
                 parameters: { delay: 1000 },
             });
-            await BackgroundService.updateNotification({ taskDesc: `step : loading...` });
+            this.setState({ isSubState: true })
         }
     }
 
@@ -64,47 +64,49 @@ class Canvas extends React.Component {
         const { delay } = taskDataArguments;
 
         await new Promise(async (resolve) => {
+            console.log("veryIntensiveTask : BackgroundService.isRunning() : " + BackgroundService.isRunning())
+            console.log("veryIntensiveTask : this.state.subscription : " + this.state.subscription)
 
-            for (let i = 0; BackgroundService.isRunning(); i++) {
-                let asStepData = await MyAsyncStorage._getAsyncStorage("AS_STEP_CNT");
-                if (asStepData === null) { asStepData = { mStep: 0, prevSpeed: 0 } }
-                asStepData.mStep = asStepData.mStep + 1;
-                await MyAsyncStorage._writeAsyncStorage("AS_STEP_CNT", asStepData);
-                BackgroundService.updateNotification({ taskDesc: `step : ${asStepData.mStep}` });
-                
-                await new Promise(resolve => setTimeout(() => resolve(), delay));
+            if (this.state.subscription === null) {
+                this.state.subscription = accelerometer
+                    .pipe(map(({ x, y, z }) => x + y + z), filter(speed => speed > -10))
+                    .subscribe(
+                        speed => {
+                            // for (let i = 0; BackgroundService.isRunning(); i++) {
+                            //     let asStepData = await MyAsyncStorage._getAsyncStorage("AS_STEP_CNT");
+                            //     if (asStepData === null) { asStepData = { mStep: 0, prevSpeed: 0, isActive: true } }
+                            //     asStepData.mStep = asStepData.mStep + 1;
+                            //     await MyAsyncStorage._writeAsyncStorage("AS_STEP_CNT", asStepData);
+
+                            //     console.log("asStepData.isActive : " + (asStepData.isActive === true))
+                            //     if (asStepData.isActive) {
+                            //         this._updateScreen(asStepData)
+                            //     }
+
+                            //     await new Promise(resolve => setTimeout(() => resolve(), delay));
+                            // }
+
+                            if (BackgroundService.isRunning()) {
+                                let curSpeed = Math.abs(this.state.prevSpeed - Number(speed));
+                                this.state.prevSpeed = Number(speed)
+
+                                if (parseInt(String(curSpeed)) > 0) {
+                                    this.state.mStep = this.state.mStep + 1
+                                    this.setState({ stepCnt: this.state.mStep, isSubState: true })
+                                    // console.log(`You moved your phone with speed: ${curSpeed} / ${stepCnt}`)
+                                }
+
+                                BackgroundService.updateNotification({ taskDesc: `step : ${this.state.mStep}` });
+
+                            } else {
+                                this._stopWalk()
+                            }
+                        },
+                        error => {
+                            Alert.alert('', "The sensor is not available")
+                        }
+                    );
             }
-
-            // if (this.state.subscription === null) {
-            //     this.state.subscription = accelerometer
-            //         .pipe(map(({ x, y, z }) => x + y + z), filter(speed => speed > -10))
-            //         .subscribe(
-            //             async (speed) => {
-            //                 if (BackgroundService.isRunning()) {
-            //                     let asStepData = await MyAsyncStorage._getAsyncStorage("AS_STEP_CNT");
-            //                     if (asStepData === null) { asStepData = { mStep: 0, prevSpeed: 0 } }
-
-            //                     let curSpeed = Math.abs(asStepData.prevSpeed - Number(speed));
-            //                     asStepData.prevSpeed = Number(speed)
-
-            //                     if (parseInt(String(curSpeed)) > 0) {
-            //                         asStepData.mStep = asStepData.mStep + 1;
-            //                         await MyAsyncStorage._writeAsyncStorage("AS_STEP_CNT", asStepData);
-            //                         // this.setState({ stepCnt: this.state.mStep, isSubState: true })
-            //                         // console.log(`You moved your phone with speed: ${curSpeed} / ${stepCnt}`)
-            //                     }
-
-            //                     BackgroundService.updateNotification({ taskDesc: `step : ${asStepData.mStep}` });
-
-            //                 } else {
-            //                     this._stopWalk()
-            //                 }
-            //             },
-            //             error => {
-            //                 Alert.alert('', "The sensor is not available")
-            //             }
-            //         );
-            // }
         });
     };
 
@@ -116,8 +118,7 @@ class Canvas extends React.Component {
 
 
     render() {
-        const { loading } = this.state;
-        // const { loading, stepCnt } = this.state;
+        const { loading, stepCnt, isSubState } = this.state;
 
         return (
             <SafeAreaView style={{ flex: 1, backgroundColor: '#ffffff' }}>
@@ -126,17 +127,17 @@ class Canvas extends React.Component {
 
                         <ScrollView contentContainerStyle={{ flexGrow: 1, alignItems: 'center', justifyContent: 'center' }}
                             keyboardShouldPersistTaps='handled'>
-                            {/* <Text style={{ fontSize: 36, color: '#000000', }}>{stepCnt}</Text> */}
+                            <Text style={{ fontSize: 36, color: '#000000', }}>{stepCnt}</Text>
 
                             <TouchableOpacity style={{ width: 200, height: 50, backgroundColor: '#6e37e6', borderRadius: 25, marginTop: 30, justifyContent: 'center', alignItems: 'center' }}
                                 onPress={() => {
-                                    if (BackgroundService.isRunning()) {
+                                    if (isSubState) {
                                         this._stopWalk()
                                     } else {
                                         this._startWalk()
                                     }
                                 }}>
-                                <Text style={{ color: '#ffffff', fontSize: 26 }}>{BackgroundService.isRunning() ? '중지' : '시작'}</Text>
+                                <Text style={{ color: '#ffffff', fontSize: 26 }}>{isSubState ? '중지' : '시작'}</Text>
                             </TouchableOpacity>
                         </ScrollView>
                     )
